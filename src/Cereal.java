586faab15d3bb9f5a4866d7756942f2981c8839d
jeffreyli1919 <70667800+jeffreyli1919@users.noreply.github.com>
2021-11-08 20:37:43 -0800

    final commit from home 11/8/21

--- src/Cereal.java
+++ src/Cereal.java
@@ -1,3 +1,40 @@
+import java.util.ArrayList;
+
 public class Cereal {
+	private String cerealName;
+	private int sugar;
+
+	
+	public Cereal(String cerealName) {
+		this.cerealName = cerealName;
+	}
+	public Cereal(String cerealName, int sugar) {
+		this.cerealName = cerealName;
+		this.sugar = sugar;
+	}
+	
+	public String mostSugaramount(ArrayList<Cereal> list) {
+		int maxVal = list.get(0).getSugar();
+		String cerealName = "";
+		
+			for (int i = 0; i < list.size(); i++) {
+				if(list.get(i).getSugar() > maxVal) {
+					maxVal = list.get(i).getSugar();
+					cerealName = list.get(i).getCerealName();
+				}
+			}
+			return cerealName;
+	}
+	
+	public String getCerealName() {
+		return cerealName;
+	}
+	
+	public int getSugar() {
+		return sugar;
+	}
+	
+
+	
 
 }
---
+++ src/Runner.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cereal cerealObj = new Cereal("Cheerios");
+		
+		ArrayList<Cereal> list = new ArrayList<Cereal>();
+		mostSugarAmount(list);
+		
+
+	}
+
+}
